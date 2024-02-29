40533eb13eee87dbb0a6eb24ab8aecfbebb67d96
Code4AppiOS <742864542@qq.com>
2024-02-29 23:31:04 +0800

    git init

---
+++ mall-gateway/src/test/java/com/x/mallgateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.mallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/x/mallware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.mallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
