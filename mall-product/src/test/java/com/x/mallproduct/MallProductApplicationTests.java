c3749ab649fde6cc6f203774c2a88cdaf74f1a0e
Code4AppiOS <742864542@qq.com>
2024-03-05 01:18:56 +0800

    init: nacos注册中心

--- mall-product/src/test/java/com/x/mallproduct/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/x/mallproduct/MallProductApplicationTests.java
@@ -1,5 +1,6 @@
 package com.x.mallproduct;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.x.mallproduct.MallProductApplication;
 import com.x.mallproduct.entity.BrandEntity;
 import com.x.mallproduct.service.BrandService;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {MallProductApplication.class})
 public class MallProductApplicationTests {
@@ -19,9 +22,13 @@ public class MallProductApplicationTests {
     @Test
     public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
+        // 插入
         brandEntity.setBrandId(6L);
         brandEntity.setDescript("mac yyds");
         brandService.updateById(brandEntity);
+        // 查询
+        List<BrandEntity> brandEntityList = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",1L));
+        brandEntityList.forEach(System.out::println);
         System.out.println("save success...");
     }
 }
